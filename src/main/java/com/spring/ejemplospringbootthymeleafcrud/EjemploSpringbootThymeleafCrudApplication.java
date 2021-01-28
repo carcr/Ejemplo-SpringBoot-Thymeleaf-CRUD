60e49b9f222b5edaa61c928478a5056335449f23
carcr <carlos.casas.rivera@gmail.com>
2021-01-28 18:38:23 +0100

    InitProyecto

---
+++ src/main/java/com/spring/ejemplospringbootthymeleafcrud/EjemploSpringbootThymeleafCrudApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ejemplospringbootthymeleafcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploSpringbootThymeleafCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploSpringbootThymeleafCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/ejemplospringbootthymeleafcrud/EjemploSpringbootThymeleafCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.ejemplospringbootthymeleafcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploSpringbootThymeleafCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
