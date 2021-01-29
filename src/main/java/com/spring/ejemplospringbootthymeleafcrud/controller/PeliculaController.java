2ecc97a97526e748bcb6acc1eed323dcfdd747a3
carcr <carlos.casas.rivera@gmail.com>
2021-01-29 13:21:49 +0100

    Added Controller and Views(CRUD). List,create,update,delete

--- src/main/java/com/spring/ejemplospringbootthymeleafcrud/controller/PeliculaController.java
+++ src/main/java/com/spring/ejemplospringbootthymeleafcrud/controller/PeliculaController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -31,6 +32,20 @@ public class PeliculaController {
     @PostMapping("/savePelicula")
     public String savePelicula(@ModelAttribute("pelicula") Pelicula pelicula){
         peliculaService.savePelicula(pelicula);
+        return "redirect:/";
+    }
+
+
+    @GetMapping("/showFormUpdate/{id}")
+    public String showFormUpdate(@PathVariable(value = "id")long id,Model model){
+        Pelicula pelicula=peliculaService.getPeliculaById(id);
+        model.addAttribute("pelicula",pelicula);
+        return "update_pelicula";
+    }
+
+    @GetMapping("/deletePelicula/{id}")
+    public String deletePelicula(@PathVariable(value = "id")long id){
+        peliculaService.deletePeliculaById(id);
         return "redirect:/";
     }
 }
